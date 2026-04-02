c461e9d39bc6f32d1cb09a26f639b95b03dd319d
jci02 <j-c.iyke1@outlook.com>
2026-04-02 22:51:51 +0200

    ctd

---
+++ 1.Supervised Learning/1.Regression/2.Multiple linear Regression/Multiple linear Regression.py
@@ -0,0 +1,12 @@
+class LinReg:
+
+    fit_intercept=None
+    tol = 3
+    
+    def __init__(self,fit_intercept=True,tol=1e-06,n_iter=None):
+        self.coefficients_ = None
+        self.intercept_ = None
+        self.is_fitted_ = False
+
+    def fit(X,y):
+        if is_instance()
