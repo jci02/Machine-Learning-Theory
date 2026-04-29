44c064c8db46d0ed336a099d8e50e7ae95f93c09
jci02 <j-c.iyke1@outlook.com>
2026-04-29 18:03:59 +0200

    ctd

--- 1.Supervised Learning/2.Classification/6.Descision Trees Classifier/train and test.py
+++ 1.Supervised Learning/2.Classification/6.Descision Trees Classifier/train and test.py
@@ -6,7 +6,7 @@ from DecisionTreeClassifier import DecisionTree
 data = datasets.load_breast_cancer()
 X, y = data.data, data.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1758)
 
 clf = DecisionTree(max_depth=10)
 clf.fit(X_train, y_train)
--- 1.Supervised Learning/2.Classification/7.Random Forest Classifier/train and test.py
+++ 1.Supervised Learning/2.Classification/7.Random Forest Classifier/train and test.py
@@ -8,15 +8,13 @@ data = datasets.load_breast_cancer()
 X = data.data
 y = data.target
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=1234
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1759)
 
 def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
 
-clf = RandomForest(n_trees=20)
+clf = RandomForestClassifier(n_trees=3)
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 
